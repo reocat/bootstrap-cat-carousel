0b7b07cd65f7791b41380585a5477bc8ffd9ca44
reocat <111279389+reocat@users.noreply.github.com>
2023-04-27 15:19:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,64 @@
 $(document).ready(function() {
-  var currentAPI = 'catapi';
-
-  // Load initial images
-  loadImages(currentAPI, 5);
-
-  // Listen for carousel slide events
-  $('#carousel').on('slid.bs.carousel', function() {
+  // Load initial set of images
+  var apiUrl = getApiUrl();
+  loadImages(apiUrl);
+  
+  // Listen for the "slid" event on the carousel
+  $('#cat-carousel').on('slid.bs.carousel', function() {
+    // Check if we're at the last item in the carousel
     var $items = $('.carousel-item');
-    var currentIndex = $items.index($('.active')[0]);
+    var currentIndex = $items.index($('.active'));
     if (currentIndex === $items.length - 1) {
-      // Load more images if we're on the last item
-      loadImages(currentAPI, 5);
+      // We're at the last item, so load a new set of images
+      var apiUrl = getApiUrl();
+      loadImages(apiUrl);
     }
   });
 
-  // Listen for switch button clicks
-  $('#switch-btn').on('click', function() {
-    if (currentAPI === 'catapi') {
-      currentAPI = 'neko';
-      $(this).text('Switch to Cat API');
-    } else {
-      currentAPI = 'catapi';
-      $(this).text('Switch to Neko API');
-    }
-    // Load new images using the current API
-    loadImages(currentAPI, 5);
+  // Listen for the "click" event on the button
+  $('#api-toggle-button').on('click', function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var apiParam = urlParams.get('catapi') || urlParams.get('neko');
+    var newApiParam = apiParam === '1' ? '0' : '1';
+    urlParams.set(apiParam === '1' ? 'catapi' : 'neko', newApiParam);
+    window.location.search = urlParams.toString();
   });
 });
 
-function loadImages(api, limit) {
-  var apiURL = '';
-  if (api === 'catapi') {
-    apiURL = 'https://api.thecatapi.com/v1/images/search?limit=' + limit;
-  } else if (api === 'neko') {
-    apiURL = 'https://nekos.life/api/v2/img/neko?count=' + limit;
+function getApiUrl() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var catApiParam = urlParams.get('catapi');
+  if (catApiParam === '1') {
+    return 'https://api.thecatapi.com/v1/images/search?limit=10';
+  } else {
+    var nekoApiParam = urlParams.get('neko');
+    if (nekoApiParam === '1') {
+      return 'https://nekos.life/api/v2/img/neko';
+    } else {
+      return 'https://api.thecatapi.com/v1/images/search?limit=10';
+    }
   }
+}
+
+function loadImages(apiUrl) {
   $.ajax({
-    url: apiURL,
+    url: apiUrl,
     dataType: 'json',
     success: function(data) {
-      // Clear the carousel
+      // Clear out the existing carousel items
       $('.carousel-inner').empty();
       
-      // Add a new item for each image
-      $.each(data, function(index, cat) {
+      // Add the new images to the carousel
+      for (var i = 0; i < data.length; i++) {
         var $item = $('<div>').addClass('carousel-item');
-        if (index === 0) {
-          $item.addClass('active');
-        }
-        var $img = $('<img>').addClass('d-block w-100').attr('src', cat.url).attr('alt', 'Random cat image');
+        var $img = $('<img>').addClass('d-block w-100').attr('src', data[i].url);
         $item.append($img);
         $('.carousel-inner').append($item);
-      });
-
-      // Refresh carousel
-      $('.carousel').carousel('pause').removeData().carousel({
-        interval: false,
-        ride: false
-      });
-    },
-    error: function(xhr, textStatus, errorThrown) {
-      console.log('Error loading images: ' + errorThrown);
+      }
+      
+      // Set the first item as active and show the carousel
+      $('.carousel-item:first').addClass('active');
+      $('#cat-carousel').show();
     }
   });
 }
